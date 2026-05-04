5fc75006787a6a4a0983886d3f0119711b3615c3
Simon Kadisch <simon.kadisch@k13-engineering.at>
2026-05-04 12:59:20 +0200

    Skip tests if run with missing permissions

--- test/basic.ts
+++ test/basic.ts
@@ -20,20 +20,32 @@ describe("devnode", () => {
     "METHOD_MOUNT_NAMESPACE_TMPFS"
   ] as TMethod[]).forEach((method) => {
     describe(`method ${method}`, () => {
-      it("should read from /dev/zero (1:5) correctly", async () => {
-        const fh = await devnode.open({
-          method,
-          type: "character",
-          major: 1,
-          minor: 5,
-          flags: "r"
-        });
-
+      // eslint-disable-next-line no-restricted-syntax
+      it("should read from /dev/zero (1:5) correctly", async function () {
         try {
-          const result = await fh.read(new Uint8Array(32), 0, 32, 0);
-          assert(isBufferZero(result.buffer));
-        } finally {
-          await fh.close();
+          const fh = await devnode.open({
+            method,
+            type: "character",
+            major: 1,
+            minor: 5,
+            flags: "r"
+          });
+
+          try {
+            const result = await fh.read(new Uint8Array(32), 0, 32, 0);
+            assert(isBufferZero(result.buffer));
+          } finally {
+            await fh.close();
+          }
+        } catch (ex) {
+          const err = ex as Error;
+
+          if (err.message === "Operation not permitted") {
+            // eslint-disable-next-line fp/no-this
+            this.skip();
+          }
+
+          throw ex;
         }
       });
     });
